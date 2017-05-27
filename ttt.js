e6053f162b3ef409d93580017f678d01d183f89b
beast257 <beast257@users.noreply.github.com>
2017-05-27 21:08:47 +0100

    Highlight the winning squares
    This commit ensures the three squares responsible for winning the game are
    highlighted. The core of the change is in the `calculateWinner` function,
    which now returns not only the player who has won, but also the indices of
    the squares that determined that result. This "winning line" and the
    winner are both then tracked on the game state.
    
    The winner state, initially undefined since it will be defined when
    needed, is used in place of recalculating the winner during rendering of
    the game. Note that to avoid preventing players playing once a history has
    been overwritten, the check for whether to return early from the board
    click handler had to expand to include a check that the currently selected
    step is the latest. This also allows the winner to continue to be printed
    even if the history is rolled back - until the history is overwritten that
    is.
    
    The winning line is passed down to the board. Each square is then told
    whether it is in the winning line by checking if its index is in the array
    - a reduction is used to avoid an explicit loop. If a square is in the
    winning line, it gets a new class. The new class currently applies an
    animation that changes the font colour to gold and back again. Gold is a
    fairly universal winner's colour after all.
    
    Note that there have also been some minor changes to variable declaration
    in the squares' click handlers, to try and keep the definition order
    logical, and minimise unneeded definitions.

--- ttt.js
+++ ttt.js
@@ -5,8 +5,9 @@ const humanReadableCoordinates = [
 ];
 
 function Square(props) {
+  const className = "square" + (props.inWinningLine ? " winner" : "");
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={className} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -14,10 +15,15 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
+    let inWinningLine = this.props.winningLine.reduce(
+      (accumulator, value) => (accumulator + (value === i ? 1 : 0)),
+      0
+    );
     return (
       <Square
         key={i}
         value={this.props.squares[i]}
+        inWinningLine={inWinningLine > 0}
         onClick={() => this.props.onClick(i)}
       />
     );
@@ -117,20 +123,25 @@ class Game extends React.Component {
         }
       ],
       stepNumber: 0,
+      winningLine: [],
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const stepNumber = history.length;
-    const squares = current.squares.slice();
-    const player = this.state.xIsNext ? "X" : "O";
-    if (calculateWinner(squares) || squares[i]) {
+    const stepNumber = this.state.stepNumber + 1;
+    const history = this.state.history.slice(0, stepNumber);
+    const current = history[stepNumber - 1];
+    if (this.state.winner && stepNumber === this.state.history.length) {
       return;
     }
+    const squares = current.squares.slice();
+    if (squares[i]) {
+      return;
+    }
+    const player = this.state.xIsNext ? "X" : "O";
     squares[i] = player;
+    const winDetails = calculateWinner(squares);
     this.setState({
       history: history.concat([
         {
@@ -139,13 +150,17 @@ class Game extends React.Component {
         }
       ]),
       stepNumber: stepNumber,
+      winner: winDetails.winner,
+      winningLine: winDetails.winningLine,
       xIsNext: !this.state.xIsNext
     });
   }
 
   jumpTo(step) {
+    const winDetails = calculateWinner(this.state.history[step].squares.slice());
     this.setState({
       stepNumber: step,
+      winningLine: winDetails.winningLine,
       xIsNext: step % 2 ? false : true
     });
   }
@@ -153,7 +168,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = this.state.winner;
 
     let status;
     if (winner) {
@@ -168,6 +183,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={i => this.handleClick(i)}
+            winningLine={this.state.winningLine}
           />
         </div>
         <div className="game-info">
@@ -201,8 +217,14 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        winningLine: [a, b, c]
+      };
     }
   }
-  return null;
+  return {
+    winner: null,
+    winningLine: []
+  };
 }
